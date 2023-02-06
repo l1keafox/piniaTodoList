8727205f833507e79dc44a6d364ef66dfcbcfeb4
Raymond Lewis <raymond.ed.lewis@gmail.com>
2023-02-06 15:14:30 -0700

    first commit

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+
+</script>
+
+<template>
+	<div class="todo-app">
+        <h1> To Do List</h1>
+        <TodoForm></TodoForm>
+        <TodoList></TodoList>
+    </div>
+</template>
+
+<style scoped>
+</style>
